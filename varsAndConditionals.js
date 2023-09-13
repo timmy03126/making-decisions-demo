7ab7cf4ddc7f6848f4b7d0616aae16a4ccb440e9
Timothy Childs <timmy03126@gmail.com>
2023-09-13 14:30:30 -0400

    initial commit

---
+++ varsAndConditionals.js
@@ -0,0 +1,17 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35
+
+if (jonSnowAttack > jamieLannisterAttack) {
+console.log(`Jon Snow has better attack than Jamie Lannister`)
+} else if  (jamieLannisterAttack > jonSnowAttack) {
+    console.log(`Jamie Lannister has better attack than Jon Snow`)
+} else {
+    console.log(`Our fighters are evenly matched`)
+}
+
